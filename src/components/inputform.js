e7ef2a001293477eace494d87b9e99ecad004854
Harsha M Shiva <HMS@Harshas-MacBook-Pro.local>
2019-10-25 01:14:40 -0500

    Working With proper image and Code

--- src/components/inputform.js
+++ src/components/inputform.js
@@ -5,8 +5,8 @@ const Form = (props) => {
     return (
         <div>
             <form onSubmit={props.loadWeather}>
-                <input type="text" name="city" placeholder="Choose a city"/>
-                <input type="text" name="country" placeholder="Choose a country"/>
+                <input type="text" name="city" placeholder="Choose a city"/><br></br>
+                <input type="text" name="country" placeholder="Choose a country"/><br/>
                 <button>Get My Weather</button>
             </form>
         </div>
